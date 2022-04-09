97d60bc1bb7525fcb4d4d121314000cb5e349d86
Justin <justinli.9876@gmail.com>
2022-04-09 17:21:26 -0400

    Add filtering by genre for random commands

--- bot/eight_ball.py
+++ bot/eight_ball.py
@@ -6,8 +6,9 @@ import random
 import requests
 from dotenv import load_dotenv
 from discord.ext import commands
-from personalize import get_name, get_friends
-
+from personalize import get_name
+from typing import Final
+from fuzzy_match import genre_match
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -15,6 +16,7 @@ API = os.getenv('MAGIC_EIGHT_BALL_API')
 intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix='$', intents=intents)
+NON_MIN_QUERY_PARAMS: Final = ['country', 'size', 'genres']
 
 @bot.event
 async def on_ready():
@@ -35,78 +37,37 @@ async def who_streaming(ctx):
     await ctx.send(f"You're streaming {get_name(streamer)}")
 
 @bot.command(name='randommovie', help='Gives you a random movie to watch. If no arguments are passed, we pick a random movie made after 1990 with a rating of 7+')
-async def random_movie(ctx, top, *args):
+async def random_movie(ctx, *args):
+    URL = f'{API}/api/movies/random'
     request = {
         'type': 'movie',
-        'top': top
+        'size': 100
     }
-    URL = f'{API}/api/movies/random'
-    PARAMS = { 'size': top }
-
-    for arg in args:
-        key = arg.split('=')[0]
-        value = arg.split('=')[1]
-        if key != 'country':
-            PARAMS[f'min{key.capitalize()}'] = value
-        else:
-            PARAMS[key] = value
-    print(PARAMS)
     try:
-        res = requests.get(url = URL, params = PARAMS)
+        await ctx.send(user_request_response(ctx.author, request, args))
+        res = requests.get(url=URL, params=build_params(args))
         data = res.json()
-        response, embed = response_builder(ctx.author, request, args, data)
-        await ctx.send(response)
-        await ctx.send(embed=embed)
+        await ctx.send(embed=motion_picture_embed(ctx.author, data))
     except requests.exceptions.RequestException as e:
         await ctx.send("No movies found with those criteria")
 
-
 @bot.command(name='randomtvshow', help='Gives you a random TV show to watch. If no arguments are passed, we pick a random TV show made after 1990 with a rating of 7+')
-async def random_tv_show(ctx, top, *args):
-    request = {
-        'type': 'tvSeries',
-        'top': top
-    }
+async def random_tv_show(ctx, *args):
     URL = f'{API}/api/tvshows/random'
-    PARAMS = { 'size': top }
-
-    for arg in args:
-        key = arg.split('=')[0]
-        value = arg.split('=')[1]
-        if key != 'country':
-            PARAMS[f'min{key.capitalize()}'] = value
-        else:
-            PARAMS[key] = value
-    print(PARAMS)
+    request = {
+        'type': 'movie',
+        'size': 100
+    }
     try:
-        res = requests.get(url = URL, params = PARAMS)
+        await ctx.send(user_request_response(ctx.author, request, args))
+        res = requests.get(url=URL, params=build_params(args))
         data = res.json()
-        response, embed = response_builder(ctx.author, request, args, data)
-        await ctx.send(response)
-        await ctx.send(embed=embed)
+        await ctx.send(embed=motion_picture_embed(ctx.author, data))
     except requests.exceptions.RequestException as e:
         await ctx.send("No TV shows found with those criteria")
     
 
-def response_builder(author, request, args, data):
-    response = f"✨ Your request is in {get_name(author)} ✨ \n\n"
-    response += f"You want to watch a random {request['type']} out of the top {request['top']} movies on **IMDB**...\n"
-    for arg in args:
-        key = arg.split('=')[0]
-        value = arg.split('=')[1]
-        if arg == args[-1]:
-            response += 'and '
-        if key == 'year':
-            response += f'made after {value} 🗓 ...\n'
-        elif key == 'runtime':
-            response += f'with a minimum {key} of {value} ⏱ minutes...\n'
-        elif key == 'country':
-            name, emoji = get_country_info(value)
-            response += f'from {name} {emoji}'
-        else:
-            response += f'a minimum {key} of {value}\n'
-    response += f'\n\n🥁 **Your random {request["type"]} is... **🥁\n\n'
-
+def motion_picture_embed(author, data):
     embed = discord.Embed(title=f"🍿 **{data['title']}** 🎬", description="", color=0x00ff00)
     embed.set_author(name=f"{get_name(author)}", url="", icon_url=author.avatar_url)
     embed.add_field(name="\u200b", value=f"{data['plot']}", inline=False)
@@ -120,9 +81,31 @@ def response_builder(author, request, args, data):
     embed.add_field(name="Awards 🏆", value=f"{data['awards']}", inline=False)
     embed.set_image(url=data['poster'])
     embed.add_field(name="\u200b", value="Contribute to this bot [here](https://github.com/JustinLiAtSBU/magic-eight-ball-bot)")
+    return embed
 
-    return response, embed
 
+def user_request_response(author, request, args):
+    response = f"✨ Your request is in {get_name(author)} ✨ \n\n"
+    response += f"You want to watch a random {request['type']} out of the top {request['size']} movies on **IMDB**...\n"
+    for arg in args:
+        key = arg.split('=')[0]
+        value = arg.split('=')[1]
+        if arg == args[-1]:
+            response += 'and '
+        if key == 'year':
+            response += f'made after {value} 🗓 ...\n'
+        elif key == 'runtime':
+            response += f'with a minimum {key} of {value} ⏱ minutes...\n'
+        elif key == 'country':
+            name, emoji = get_country_info(value)
+            response += f'from {name} {emoji}\n'
+        elif key == 'genres':
+            genres = genre_match(value.split(','))
+            response += f"with the genre {', '.join(genres)}" if len(genres) == 1 else f"with the genres {', '.join(genres)}"
+        else:
+            response += f'a minimum {key} of {value}\n'
+    response += f'\n\n🥁 **Your random {request["type"]} is... **🥁\n\n'
+    return response
 
 def get_country_info(country_code):
     country = pycountry.countries.get(alpha_2=country_code)
@@ -142,6 +125,17 @@ def get_common_time(time):
     if minutes:
         common_time += f"and {minutes} minutes"
     return common_time
+
+
+def build_params(args):
+    PARAMS = { 'size': 100 }
+    for arg in args:
+        key, value = arg.split('=')
+        if key not in NON_MIN_QUERY_PARAMS:
+            PARAMS[f'min{key.capitalize()}'] = value
+        else:
+            PARAMS[key] = value
+    return PARAMS
 
 
 bot.run(TOKEN)
