3aaaaa0944c65361e4f5d00f3d83843388629519
Justin <justinli.9876@gmail.com>
2022-04-18 18:08:09 -0400

    Add requester for creating channels

--- bot/requester.py
+++ bot/requester.py
@@ -9,17 +9,24 @@ PATH_MAP: Final = {
     'TV show': 'tvshows/random',
     'anime': 'tvshows/random',
     'anime movie': 'movies/random',
+    'create/update channel': 'channels/create',
 }
 NON_MIN_QUERY_PARAMS: Final = ['country', 'top', 'genres']
 
 
-def build_and_send_request(request, args):
+async def random_motion_picture_request(request, args):
     url = f"{API}/{PATH_MAP[request['type']]}"
     print("REQUEST =================")
     print(url)
     print(args)
     res = requests.get(url=url, params=build_params(args))
     return res.json()
+
+async def create_channel_request(request, params, body):
+    url = f"{API}/{PATH_MAP[request['type']]}"
+    res = requests.post(url=url, params=params, json=body)
+    print(f"Channel {params['channelName']} created/updated")
+    return res
 
 def build_params(args):
     PARAMS = {}
